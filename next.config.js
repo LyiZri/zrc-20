7675504c4ad3e94ca3ef890cedbdab871f74dc5b
LyiZri <92965068+LyiZri@users.noreply.github.com>
2023-12-24 21:13:53 +0800

    feat:setting sats to rats

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config, context) => {
-        if (config.plugins) {
-          config.plugins.push(
-            new context.webpack.IgnorePlugin({
-              resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
-            }),
-          )
-        }
-        return config
+    experimental: {
+        appDir: true,
+      },
+      webpack: (config) => {
+        config.externals.push("pino-pretty", "lokijs", "encoding");
+        return config;
       },
 };
 
